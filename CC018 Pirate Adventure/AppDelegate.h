9ab99f454bd466684c7ffc0e292a0e51afd8fdb7
systemtest <systemtest@Profile-Manager.local>
2015-05-27 09:08:27 +0530

    Initial Commit

---
+++ CC018 Pirate Adventure/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  CC018 Pirate Adventure
+//
+//  Created by systemtest on 27/05/15.
+//  Copyright (c) 2015 PurpleTalk. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ CC018 Pirate Adventure/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  CC018 Pirate Adventure
+//
+//  Created by systemtest on 27/05/15.
+//  Copyright (c) 2015 PurpleTalk. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
